6fda2b736770e399d63d9cae998d94b84ca14d74
Mateus <mateus_freitascorreia@hotmail.com>
2024-02-11 20:29:33 -0300

    matriz impl 2

---
+++ aula[06]/matriz.cpp
@@ -0,0 +1,88 @@
+#ifndef MATRIZ_CPP
+#define MATRIZ_CPP
+
+#include "matriz.h"
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+
+struct matriz
+{
+    int linhas;
+    int colunas;
+    float **valor;
+};
+
+Matriz* cria_matriz(int linhas, int colunas)
+{
+    Matriz* m = (Matriz*)malloc(sizeof(matriz));
+    
+    if(m){
+        m->linhas = linhas;
+        m->colunas = colunas;
+        m->valor = (float**)malloc(linhas*sizeof(float*));
+        
+        for (int i = 0; i < linhas; i++){
+            m->valor[i] = (float*)malloc(colunas*sizeof(float));
+        }
+        
+        return m;
+    }else{
+        return NULL;
+    }
+}
+
+void inicializa_matriz(Matriz* m)
+{
+    srand(time(NULL));
+    
+    for (int i = 0; i < m->linhas; i++){
+        for (int j = 0; j < m->colunas; j++){
+            m->valor[i][j] = rand() % 100;
+        }
+    }
+}
+
+void libera_matriz(Matriz** m)
+{
+    if(*m){
+        for (int i = 0; i < (*m)->linhas; i++){
+            free((*m)->valor[i]);
+        }
+        free((*m)->valor);
+        free(*m);
+        *m = NULL;
+    }
+}
+
+bool acessa_matriz(Matriz* m, int i, int j, float* valor)
+{
+    if(i < 0 || i >= m->linhas || j < 0 || j >= m->colunas){
+        return false;
+    }
+    
+    *valor = m->valor[i][j];
+    return true;
+}
+
+bool atribui_matriz(Matriz* m, int i, int j, float valor)
+{
+    if(i < 0 || i >= m->linhas || j < 0 || j >= m->colunas){
+        return false;
+    }
+    
+    m->valor[i][j] = valor;
+    return true;
+}
+
+void imprime_matriz(Matriz* m)
+{
+    for (int i = 0; i < m->linhas; i++){
+        for (int j = 0; j < m->colunas; j++){
+            std::cout << m->valor[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+#endif
